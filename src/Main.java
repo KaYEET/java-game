1bfc747e62692149c38fe8a9c97e5a203215231d
KaYEET <kai.ekkart@gmail.com>
2023-01-03 23:34:52 +0100

    Final changes done and added some classes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     private Room currentRoom;
+    private Parser parser;
+
+    public Main() {
+        createRooms();
+        parser = new Parser();
+    }
 
     private void play() {
         startScreen();
-
     }
 
     private void startScreen() {
@@ -22,9 +27,6 @@ public class Main {
         System.out.println();
     }
 
-    public Main() {
-        createRooms();
-    }
 
     private void createRooms() {
         Room street, ally, car, policeStation, prison, park, garage, hallway, room, nightshop;
@@ -50,13 +52,93 @@ public class Main {
         hallway.setExits(null, room, street, garage);
         room.setExits(null, null, null, hallway);
         nightshop.setExits(null, null, street, null);
+        currentRoom = hallway;
     }
 
+    private void printHelp() {
+        System.out.println("Usable commands");
+        System.out.println("-- go quit help --");
+    }
+
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
+            System.out.println("If you want to quit the game, please only type \"quit\"");
+            return false;
+        } else {
+            return true;  // signal that we want to quit
+        }
+    }
+
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        // Try to leave current room.
+        Room nextRoom = null;
+
+
+        if (direction.equals("north")) {
+            nextRoom = currentRoom.north;
+        }
+        if (direction.equals("east")) {
+            nextRoom = currentRoom.east;
+        }
+        if (direction.equals("south")) {
+            nextRoom = currentRoom.south;
+        }
+        if (direction.equals("west")) {
+            nextRoom = currentRoom.west;
+        }
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        } else {
+            currentRoom = nextRoom;
+            System.out.println("You are " + currentRoom.getDescription());
+            System.out.print("Exits: ");
+            if (currentRoom.north != null) {
+                System.out.print("north ");
+            }
+            if (currentRoom.east != null) {
+                System.out.print("east ");
+            }
+            if (currentRoom.south != null) {
+                System.out.print("south ");
+            }
+            if (currentRoom.west != null) {
+                System.out.print("west ");
+            }
+            System.out.println();
+        }
+    }
+
+    private boolean processCommand(Command command) {
+        boolean wantToQuit = false;
+
+        if (command.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+
+        String commandWord = command.getCommandWord();
+        if (commandWord.equals("help")) {
+            printHelp();
+        } else if (commandWord.equals("go")) {
+            goRoom(command);
+        } else if (commandWord.equals("quit")) {
+            wantToQuit = quit(command);
+        }
+
+        return wantToQuit;
+    }
 
     public static void main(String[] args) {
         Main game = new Main();
         game.play();
     }
-
-
 }
